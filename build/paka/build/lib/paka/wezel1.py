cd1fce7f9249d22cbe7890bb7acc98f36e60d928
TauTheLepton <matipalczuk@gmail.com>
2021-03-15 19:56:37 +0100

    dziala sterowanie z klawiatury

--- build/paka/build/lib/paka/wezel1.py
+++ build/paka/build/lib/paka/wezel1.py
@@ -14,30 +14,36 @@ class MinimalPublisher(Node):
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
         self.lin = 0
         self.ang = 0
 
-    def set_vel(self, l, a):
-        self.lin = float(l)
-        self.ang = float(a)
+    def set_vel(self, lin, ang):
+        self.lin = lin
+        self.ang = ang
 
     def timer_callback(self):
         with Input(keynames='curses') as input_generator:
             for e in input_generator:
-                self.lin = float(0)
-                self.ang = float(0)
-                if(str(e) == 'p'):
+                print(repr(e))
+                if(repr(e) == 'p'):
                     self.set_vel(1, 0)
-                elif(str(e) == 'l'):
+                elif(repr(e) == 'l'):
                     self.set_vel(-1, 0)
-                elif(str(e) == 'd'):
+                elif(repr(e) == 'd'):
                     self.set_vel(0, 1)
-                elif(str(e) == 'g'):
+                elif(repr(e) == 'g'):
                     self.set_vel(0, -1)
-                msg = Twist()
-                msg.linear.x = self.lin
-                msg.angular.z = self.ang
-                self.publisher_.publish(msg)
+        # keyboard.on_press_key('p', self.set_vel(1, 0))
+        # keyboard.on_press_key('l', self.set_vel(-1, 0))
+        # keyboard.on_press_key('d', self.set_vel(0, 1))
+        # keyboard.on_press_key('g', self.set_vel(0, -1))
+        msg = Twist()
+        msg.linear.x = self.lin
+        msg.angular.z = self.ang
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
 
 
 def main(args=None):
