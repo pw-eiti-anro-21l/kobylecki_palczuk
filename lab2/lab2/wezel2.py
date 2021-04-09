5f98621a4637843cd074183c4269dd4ed5e3a64d
tymwitko <tymon.kobylecki@gmail.com>
2021-04-09 23:56:47 +0200

    cos jeszcze probowalem ale poleglem

--- lab2/lab2/wezel2.py
+++ lab2/lab2/wezel2.py
@@ -12,14 +12,6 @@ import math
 import os
 import random
 
-# def read_from_csv(filename):
-#     dh=[]
-#     with open(filename, 'r') as file:
-#         reader = csv.reader(file)
-#         for row in reader:
-#             dh.append(row)
-#     return dh
-
 class StatePublisher(Node):
 
     def __init__(self):
@@ -30,131 +22,52 @@ class StatePublisher(Node):
         self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
         self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
         self.nodeName = self.get_name()
-        self.get_logger().info("{0} started".format(self.nodeName))
+        self.get_logger().info("{0} posed".format(self.nodeName))
 
         degree = pi / 180.0
-        loop_rate = self.create_rate(30)
+        self.loop_rate = self.create_rate(30)
 
-        self.declare_parameter(start1, 0.)
-        self.declare_parameter(start2, 0.)
-        self.declare_parameter(start3, 0.)
+        self.declare_parameter('pos1', 0.)
+        self.declare_parameter('pos2', 0.)
+        self.declare_parameter('pos3', 0.)
 
-        # dh = read_from_csv('kobylecki_palczuk/lab2/config/DH.csv')
-        # dh = dh[1:]
-        # matr = [[1,0,0,0,"base"]]
-        # for line in dh:
-        #     matr.append(line)
-        # dh = matr
-        # theta_varcount = 0
-        # for line in dh:
-        #     if line[3] == "var":
-        #         theta_varcount += 1
-
-        # states = []
-        # names = []
-        # types = []
-        # limits = []
-        # prev_line = dh[0]
-        # for line in dh:
-
-        #     joint_type = None
-        #     if line[3] == 'var':
-        #         joint_type = "revolute"
-        #     for index in range(len(str(line[0]))):
-        #         if str(line[0])[index] == ':':
-        #             joint_type = "prismatic"
-        #             prismatic_lower_limit = float(line[0][:index])
-        #             prismatic_upper_limit = float(line[0][index+1:])
-
-        #     if joint_type != None:
-        #         joint_name = prev_line[4] + "_to_" + line[4]
-        #         names.append(joint_name)
-        #         states.append(float(0))
-        #         types.append(joint_type)
-        #         if joint_type == "prismatic":
-        #             limits.append((prismatic_lower_limit, prismatic_upper_limit))
-
-        #     prev_line = line
-        # angle=0.
-        # going_back = False
-        # prismatic_going_back = False
+        self.pos1 = (self.get_parameter('pos1').get_parameter_value().double_value)
+        self.pos2 = (self.get_parameter('pos2').get_parameter_value().double_value)
+        self.pos3 = (self.get_parameter('pos3').get_parameter_value().double_value)
 
         # message declarations
-        odom_trans = TransformStamped()
-        odom_trans.header.frame_id = 'odom'
-        odom_trans.child_frame_id = 'base'
-        joint_state = JointState()
-
+        self.odom_trans = TransformStamped()
+        self.odom_trans.header.frame_id = 'odom'
+        self.odom_trans.child_frame_id = 'base'
+        self.joint_state = JointState()
+        self.timer = self.create_timer(0.1, self.update_state) 
+    def update_state(self):
         try:
-            while rclpy.ok():
-                # for i in states:
-                #     i += degree
-                # for i in range(len(states)):
-                    # if types[i] == "revolute":
-                    #     if states[i] > pi/2:
-                    #         going_back = True
-                    #     if states[i] < -pi/2:
-                    #         going_back = False
-                    #     if going_back:
-                    #         states[i] -= degree
-                    #     else:
-                    #         states[i] += degree
-                    # elif types[i] == "prismatic":
-                    #     if states[i] > limits[i][1] - limits[i][0]:
-                    #         prismatic_going_back = True
-                    #     if states[i] < 0:
-                    #         prismatic_going_back = False
-                    #     if prismatic_going_back:
-                    #         states[i] -= 0.01
-                    #     else:
-                    #         states[i] += 0.01
-                # self.get_logger().info('degree = ' + str(degree))
-                rclpy.spin_once(self)
 
-                # update joint_state
-                now = self.get_clock().now()
-                joint_state.header.stamp = now.to_msg()
-                joint_state.name = ["base_to_link1", "link1_to_link2", "link2_to_link3"]
-                joint_state.position = [self.start1, self.start2, self.start3]
-                
-                # platynowy debugger
-                # for i in names:
-                #     self.get_logger().info(str(i))
-                # for j in states:
-                #     self.get_logger().info(str(j))
-                # self.get_logger().info('')
+            # update joint_state
+            now = self.get_clock().now()
+            self.joint_state.header.stamp = now.to_msg()
+            self.joint_state.name = ["base_to_link1", "link1_to_link2", "link2_to_link3"]
+            self.joint_state.position = [self.pos1, self.pos2, self.pos3]
 
-                # update transform
-                # (moving in a circle with radius=2) DO ZMIANY
-                odom_trans.header.stamp = now.to_msg()
-                odom_trans.transform.translation.x = 0. #cos(angle)*2
-                odom_trans.transform.translation.y = 0. # sin(angle)*2
-                odom_trans.transform.translation.z = 0.
-                odom_trans.transform.rotation = euler_to_quaternion(0, 0, 0) # roll,pitch,yaw   0, 0, angle + pi/2
+            # update transform
+            # (moving in a circle with radius=2) DO ZMIANY
+            self.odom_trans.header.stamp = now.to_msg()
 
-                # send the joint state and transform
-                self.joint_pub.publish(joint_state)
-                self.broadcaster.sendTransform(odom_trans)
+            # send the joint state and transform
+            self.joint_pub.publish(self.joint_state)
+            self.broadcaster.sendTransform(self.odom_trans)
 
-                # Create new robot state
-                # tilt += tinc
-                # if tilt < -0.5 or tilt > 0.0:
-                #     tinc *= -1
-                # height += hinc
-                # if height > 0.2 or height < 0.0:
-                #     hinc *= -1
-                # swivel += degree
-                # angle += degree/4
 
-                # This will adjust as needed per iteration
-                loop_rate.sleep()
+            # This will adjust as needed per iteration
+            self.loop_rate.sleep()
 
-                if self.start1 < double(self.get_parameter("start1").get_parameter_value()):
-                    self.start1 += 0.1
-                if self.start2 < double(self.get_parameter("start2").get_parameter_value()):
-                    self.start2 += 0.1
-                if self.start3 < double(self.get_parameter("start3").get_parameter_value()):
-                    self.start3 += 0.1
+            if self.pos1 < (self.get_parameter("pos1").get_parameter_value().double_value):
+                self.pos1 += 0.1
+            if self.pos2 < (self.get_parameter("pos2").get_parameter_value().double_value):
+                self.pos2 += 0.1
+            if self.pos3 < (self.get_parameter("pos3").get_parameter_value().double_value):
+                self.pos3 += 0.1
 
         except KeyboardInterrupt:
             pass
@@ -167,97 +80,11 @@ def euler_to_quaternion(roll, pitch, yaw):
     return Quaternion(x=qx, y=qy, z=qz, w=qw)
 
 def main():
-    # create_urdf("src/kobylecki_palczuk/lab2/urdf/bogson.urdf.xml", \
-    #     "test_bogson", "src/kobylecki_palczuk/lab2/config/DH.csv")
     node = StatePublisher()
+    rclpy.spin(node)
 
 if __name__ == '__main__':
     main()
 
 
 
-# class StatePublisher(Node):
-
-#     def __init__(self):
-#         rclpy.init()
-#         super().__init__('state_publisher')
-
-#         qos_profile = QoSProfile(depth=10)
-#         self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
-#         self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
-#         self.nodeName = self.get_name()
-#         self.get_logger().info("{0} started".format(self.nodeName))
-
-#         degree = pi / 180.0
-#         self.loop_rate = self.create_rate(30)
-
-#         # robot state parameters
-#         self.declare_parameter('poz1', 0.0)
-#         self.declare_parameter('poz2', 0.0)
-#         self.declare_parameter('poz3', 0.0)
-
-#         self.poz1 = self.get_parameter('poz1').get_parameter_value().double_value
-#         self.poz2 = self.get_parameter('poz2').get_parameter_value().double_value
-#         self.poz3 = self.get_parameter('poz3').get_parameter_value().double_value
-
-#         # message declarations
-#         self.odom_trans = TransformStamped()
-#         self.odom_trans.header.frame_id = 'odom'
-#         self.odom_trans.child_frame_id = 'baza'
-#         self.joint_state = JointState()
-
-#         self.timer = self.create_timer(0.1, self.update_state)
-
-#     def update_state(self):
-
-#         try:
-#             # update joint_state
-#             now = self.get_clock().now()
-#             self.joint_state.header.stamp = now.to_msg()
-#             self.joint_state.name = ["baza_do_ramie1", "ramie1_do_ramie2", "ramie2_do_ramie3"]
-#             self.joint_state.position = [self.poz1, self.poz2, self.poz3]
-
-#             self.odom_trans.header.stamp = now.to_msg()
-
-#             # send the joint state and transform
-#             self.joint_pub.publish(self.joint_state)
-#             self.broadcaster.sendTransform(self.odom_trans)
-
-#             # This will adjust as needed per iteration
-#             self.loop_rate.sleep()
-
-#             # change params
-#             if self.poz1 < self.get_parameter('poz1').get_parameter_value().double_value:
-#                 self.poz1 += 0.05
-#             if self.poz1 >= self.get_parameter('poz1').get_parameter_value().double_value:
-#                 self.poz1 -= 0.05
-
-#             if self.poz2 < self.get_parameter('poz2').get_parameter_value().double_value:
-#                 self.poz2 += 0.05
-#             if self.poz2 >= self.get_parameter('poz2').get_parameter_value().double_value:
-#                 self.poz2 -= 0.05
-
-#             if self.poz3 < self.get_parameter('poz3').get_parameter_value().double_value:
-#                 self.poz3 += 0.01
-#             if self.poz3 >= self.get_parameter('poz3').get_parameter_value().double_value:
-#                 self.poz3 -= 0.01
-
-#         except KeyboardInterrupt:
-#             pass
-
-
-# def euler_to_quaternion(roll, pitch, yaw):
-#     qx = sin(roll / 2) * cos(pitch / 2) * cos(yaw / 2) - cos(roll / 2) * sin(pitch / 2) * sin(yaw / 2)
-#     qy = cos(roll / 2) * sin(pitch / 2) * cos(yaw / 2) + sin(roll / 2) * cos(pitch / 2) * sin(yaw / 2)
-#     qz = cos(roll / 2) * cos(pitch / 2) * sin(yaw / 2) - sin(roll / 2) * sin(pitch / 2) * cos(yaw / 2)
-#     qw = cos(roll / 2) * cos(pitch / 2) * cos(yaw / 2) + sin(roll / 2) * sin(pitch / 2) * sin(yaw / 2)
-#     return Quaternion(x=qx, y=qy, z=qz, w=qw)
-
-
-# def main():
-#     node = StatePublisher()
-#     rclpy.spin(node)
-
-
-# if __name__ == '__main__':
-#     main()
--- lab2/launch/launch2.py
+++ lab2/launch/launch2.py
@@ -2,19 +2,26 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
+
+
   use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-  urdf_file_name = 'bogson.urdf.xml'
-
-  print("urdf_file_name : {}".format(urdf_file_name))
-
-  urdf = os.path.join(
-      get_package_share_directory('lab2'),
-      urdf_file_name)
+  # urdf_file_name = 'bogson.urdf.xml'
+  rviz2_file_name = 'bogson.rviz'
+  xacro_file_name = 'bogson_fixed.urdf.xacro.xml'
+  # print(os.path.join(get_package_share_directory('lab2'), xacro_file_name))
+  # print(os.path.join(get_package_share_directory('lab2'), rviz2_file_name))
+  # rviz2 = os.path.join(get_package_share_directory('lab2'), rviz2_file_name)
+  # xacro = os.path.join(get_package_share_directory('lab2'), xacro_file_name)
+  # xacro = 'install/lab2/share/lab2/bogson_fixed.urdf.xacro.xml'
+  # print("urdf_file_name : {}".format(urdf_file_name))
+  # urdf = os.path.join(
+  #     get_package_share_directory('lab2'),
+  #     urdf_file_name)
 
   return LaunchDescription([
       DeclareLaunchArgument(
@@ -22,19 +29,21 @@ def generate_launch_description():
           default_value='false',
           description='Use simulation (Gazebo) clock if true'),
       Node(
-          package='robot_state_publisher',
-          executable='robot_state_publisher',
-          name='robot_state_publisher',
+          package='lab2', #'robot_state_publisher',
+          executable='wezel2',   #'robot_state_publisher',
+          name='wezel2',        #'robot_state_publisher',
           output='screen',
-          parameters=[{'use_sim_time': use_sim_time}],
-          arguments=[urdf]),
+          parameters=[{'use_sim_time': use_sim_time, 'robot': Command(['xacro', ' ', os.path.join(get_package_share_directory('lab2'), xacro_file_name)])}]),
+          # arguments=[urdf]),
       # Node(
       #     package='joint_state_publisher',
       #     executable='joint_state_publisher',
       #     name='joint_state_publisher'),
       Node(
-          package='lab2',
-          executable='wezel2',
-          name='wezel2',
-          output='screen'),
+          package='rviz2',
+          executable='rviz2',
+          name='bogson_rviz2',
+          output='screen',
+          parameters=[{'use_sim_time': use_sim_time,}],
+          arguments=['-d', os.path.join(get_package_share_directory('lab2'), rviz2_file_name)],)
   ])
