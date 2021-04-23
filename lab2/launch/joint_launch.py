9deb8a406d030c7c71773130e98fd075884c9f97
tymwitko <tymon.kobylecki@gmail.com>
2021-04-23 17:44:30 +0200

    DZIALAAAA (GUI)

--- lab2/launch/joint_launch.py
+++ lab2/launch/joint_launch.py
@@ -19,9 +19,9 @@ def generate_launch_description():
         #     name='move_joints',
         #     output='screen'),
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time, 'robot_description': Command(['xacro', ' ', os.path.join(get_package_share_directory('lab2'), xacro_file_name)])}]),
         Node(
