1f97a2a5992ba4a86261763c5e49ebcc960d9a6a
tymwitko <tymon.kobylecki@gmail.com>
2021-05-21 14:17:41 +0200

    poprawki ale malo

--- lab5/lab5/ikin.py
+++ lab5/lab5/ikin.py
@@ -14,16 +14,18 @@ from rclpy.clock import ROSClock
 from lab4_srv.srv import JintControlSrv, OintControlSrv
 
 
+
 class Ikin(Node):
 	def __init__(self):
         rclpy.init()
         super().__init__('ikin')
-        qos_profile = QoSProfile(depth=10)
-        self.pose_pub = self.create_publisher(PoseStamped, 'ikin_pose', qos_profile)
-        self.OintControlSrv = self.create_service(OintControlSrv, "oint_control_srv", self.interpol_callback)
-        self.nodeName = self.get_name()
-        self.get_logger().info("{0} initiated. Beep boop beep.".format(self.nodeName))
-        self.declare_params()
+        self.filename = os.path.join(get_package_share_directory('lab5'), 'DH.yaml')
+        self.params = read_from_yaml(self.filename)
+
+def read_from_yaml(filename):
+    with open(filename, 'r') as file:
+        reader = yaml.load(file, Loader=yaml.FullLoader)
+    return reader
 
 def main():
     wenzel = Ikin()
