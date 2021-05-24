a1868c4db83c51d0843eb1b2da98cb483ecf889c
TauTheLepton <matipalczuk@gmail.com>
2021-05-24 02:00:01 +0200

    nowy launch

---
+++ lab5/launch/lab5_launch.py
@@ -0,0 +1,49 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    xacro_file_name = 'bogson_move.urdf.xacro.xml'
+    p1_1 = 0.
+    p2_1 = 0.
+    p3_1 = 0.
+
+    # urdf_file_name = 'bogson.urdf.xml'
+    rviz2_file_name = 'bogson.rviz'
+    xacro_file_name = 'bogson_fixed.urdf.xacro.xml'
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='bogson_rviz2',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time,}],
+            arguments=['-d', os.path.join(get_package_share_directory('lab5'), rviz2_file_name)]),
+        Node(
+            package='lab5',
+            executable='ikin',
+            name='ikin',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'p1_1': p1_1, 'p2_1': p2_1, 'p3_1': p3_1}]),
+        Node(
+            package='lab5',
+            executable='oint',
+            name='oint',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'p1_1': p1_1, 'p2_1': p2_1, 'p3_1': p3_1}]),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': Command(['xacro', ' ', os.path.join(get_package_share_directory('lab5'), xacro_file_name)])}])
+    ])
